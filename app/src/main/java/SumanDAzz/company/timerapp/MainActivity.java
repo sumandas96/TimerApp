46607f79b7856f4725cd8c9d71721806c0730a5a
sumandas96 <sumandas.32111@gmail.com>
2020-01-23 21:04:18 +0530

    Hour adder,some logic has been changed

--- app/src/main/java/SumanDAzz/company/timerapp/MainActivity.java
+++ app/src/main/java/SumanDAzz/company/timerapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,42 +14,53 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton playButton,pauseButton,resetButton;
-    TextView textViewSec;
+    EditText textViewhourmin,textViewSec;
     CountDownTimer mcountDownTimer;
     final long Time_millis = 60000;
     long mTime_left = Time_millis;
     boolean isTimerRunning;
+    int hour,minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         playButton = findViewById(R.id.playButton);
         pauseButton = findViewById(R.id.pauseButton);
         resetButton = findViewById(R.id.resetButton);
-        textViewSec = findViewById(R.id.editMin);
+        textViewhourmin = findViewById(R.id.editMinHour);
+
+     //  textViewSec = findViewById(R.id.editsec);
+
+
 
 
 
     }
     public void updateTimer(){
-        int minutes = (int)(mTime_left/1000)/60;
-        //int seconds = secendsLeft-minutes*60;
+         hour = (int)(mTime_left/1000)/3600;
+         minutes = (int)((mTime_left/1000)%3600)/60;
         int seconds = (int)(mTime_left/1000)%60;
+        //int seconds = secendsLeft-minutes*60;
+        //int seconds = (int)(mTime_left/1000)%60 ;
 
       /*  String myString = Integer.toString(seconds);
         if(seconds <=9){
             myString = "0" + myString;
         }
 //        "00:" + "0" +Integer.toString(minutes) +*/
-        String timeFormatted;
-        if(minutes>0) {
-            timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-        }else
-        {
-            timeFormatted = String.format(Locale.getDefault(), "%02d", seconds);
+        String timeFormatted = null;
+        if(hour>0) {
+            timeFormatted = String.format(Locale.getDefault(), "%d:%02d:%02d", hour, minutes,seconds);
         }
-        textViewSec.setText(timeFormatted);
+         else
+        {
+            timeFormatted = String.format(Locale.getDefault(), "%02d:%02d",minutes,seconds);
+        }
+
+        textViewhourmin.setText(timeFormatted);
+
     }
     public void countDown(View view){
 /*
@@ -62,21 +74,22 @@ public class MainActivity extends AppCompatActivity {
 //        {
 //            textViewSec.setText(textViewSec.getText() + String.valueOf(msec/ 1000));*/
 
-        String countSec = textViewSec.getText().toString();
+        String countSec = textViewhourmin.getText().toString();
 //        String mText = ":00";
 //        long  msec = Long.parseLong(countSec)*mTime_left+Long.parseLong(mText);
-        long  msec = Long.parseLong(countSec)*mTime_left;
-        if(msec ==0 ){
+        long mtime = Long.parseLong(countSec)*mTime_left;
+        if( mtime==0 ){
             Toast.makeText(this,"Please Enter a Positive number",Toast.LENGTH_SHORT).show();
         }
 
-            mcountDownTimer = new CountDownTimer(msec,1000){
+            mcountDownTimer = new CountDownTimer(mtime,1000){
 
                 @Override
                 public void onTick(long millisUntilFinished) {
                     //updateTimer((int)millisUntilFinished/1000);
                     mTime_left = millisUntilFinished;
                     updateTimer();
+
                 }
 
                 @Override
@@ -94,9 +107,9 @@ public class MainActivity extends AppCompatActivity {
         }
         public void reset(View view){
             mTime_left = Time_millis;
-            //textViewSec.setText("00:00");
             updateTimer();
-            textViewSec.setText("00:00");
+
+
 
 
         }
